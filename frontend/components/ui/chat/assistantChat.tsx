8e7e7318a18b537770f8eedf3453340e5fe7a5fb
KIm Khoi Lam <kimkhoi2202@gmail.com>
2023-08-20 20:14:03 -0500

    change

--- frontend/components/ui/chat/assistantChat.tsx
+++ frontend/components/ui/chat/assistantChat.tsx
@@ -1,21 +1,25 @@
-import * as React from "react"
+import * as React from "react";
+import Image from "next/image";
 
 import {
   Card,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 
 export function AssistantChatCard({ text = "Hi, how can I help you?" }) {
   return (
     <div className="mb-[25px] ml-[25px] mt-[20px]">
       <Card className="w-[350px]">
         <CardHeader>
-          <CardTitle>Genie</CardTitle>
+          <Image src="/favicon.ico" alt="Favicon" width={75} height={76} />
+          <CardTitle>
+            Genie
+          </CardTitle>
           <CardDescription>{text}</CardDescription>
         </CardHeader>
       </Card>
     </div>
-  )
+  );
 }
